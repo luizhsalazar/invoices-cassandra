a2afcc5074b4cc98284d0e0c6ba6edec80840073
salazar <luizhsalazar@gmail.com>
2019-03-29 07:34:26 -0300

    PDF refactor

--- routes/invoices.route.js
+++ routes/invoices.route.js
@@ -1,27 +1,78 @@
 const express = require('express');
 const routes = express.Router();
 
-var path = require('path');
-var fs = require('fs');
-var pdf = require('html-pdf');
-var templateHtml = fs.readFileSync('./test.html', 'utf8');
-var options = { format: 'A4' };
+const fs = require('fs');
+const pdf = require('html-pdf');
+const cassandra = require('cassandra-driver');
 
-var image = path.join('file://', __dirname, 'segsoft.jpg');
-templateHtml = templateHtml.replace('{{ image }}', image);
+let templateHtml = fs.readFileSync('./test.html', 'utf8');
+const options = { format: 'A4' };
+
+const client = new cassandra.Client({
+    contactPoints: ['localhost:9042'],
+    localDataCenter: 'datacenter1',
+    keyspace: 'chat'
+});
 
 routes.route('/invoices/:invoice_number').get(function (req, res) {
     let invoice_number = req.params.invoice_number;
-    console.log('invoice number:' + invoice_number);
-    
-    res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-disposition': `attachment; filename=test.pdf`,
+    const query = 'SELECT * FROM users';
+
+    client
+    .execute(query)
+    .then(result => {
+        console.log('User with email %s', result.rows[0].email);
+        templateHtml = templateHtml.replace('{{ invoicenumber }}', 1946369);
+        templateHtml = templateHtml.replace('{{ customername }}', "Pirelli");
+        templateHtml = templateHtml.replace('{{ customeraddress }}', "Viale Piero e Alberto Pirelli n. 25 (reception in Via Bicocca degli Arcimboldi, 3) 20126 Milan");
+
+        invoices = [
+            {
+                servicedescription: "Maintenance of workflow system",
+                servicequantity: 32,
+                servicevalue: "$40.00",
+                resourcename: "Andrew S. Tanenbaum",
+                resourcefunction: "Plain",
+                servicetax: 0.25,
+                servicediscount: 0.07,
+                servicesubtotal: "$1510.39"
+            },
+            {
+                servicedescription: "asda Maintenance of workflow system",
+                servicequantity: 32,
+                servicevalue: "$40.00",
+                resourcename: "Andrew S. Tanenbaum",
+                resourcefunction: "Plain",
+                servicetax: 0.25,
+                servicediscount: 0.07,
+                servicesubtotal: "$1510.39"
+            }
+        ];
+
+        // templateHtml = templateHtml.replace('{{ invoice }}', invoice);
+
+        // templateHtml = templateHtml.replace('{{ servicedescription }}', "Maintenance of workflow system");
+        // templateHtml = templateHtml.replace('{{ servicequantity }}', 32);
+        // templateHtml = templateHtml.replace('{{ servicevalue }}', "$40.00");
+        // templateHtml = templateHtml.replace('{{ resourcename }}', "Andrew S. Tanenbaum");
+        // templateHtml = templateHtml.replace('{{ resourcefunction }}', "Plain");
+        // templateHtml = templateHtml.replace('{{ servicetax }}', "0.25");
+        // templateHtml = templateHtml.replace('{{ servicediscount }}', "0.07");
+        // templateHtml = templateHtml.replace('{{ servicesubtotal }}', "$1510.39");
+
+        templateHtml = templateHtml.replace('{{ invoicevalue }}', "$6465.39");
+
+        res.writeHead(200, {
+            'Content-Type': 'application/pdf',
+            'Content-disposition': `attachment; filename=test.pdf`,
+        });
+
+        pdf.create(templateHtml, options).toStream((err, stream) => {
+            stream.pipe(res);
+        });
     });
-    
-    pdf.create(templateHtml, options).toStream((err, stream) => {
-        stream.pipe(res);
-    });
+
+
 });
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
-    cors = require('cors');
+    cors = require('cors'),
+    nunjucks = require('nunjucks');
 
 const routes = require('./routes/invoices.route');
 
@@ -10,6 +11,13 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/', routes);
 const port = process.env.PORT || 4000;
+
+// Configure Nunjucks
+nunjucks.configure();
+
+// Set Nunjucks as rendering engine for pages with .html suffix
+// app.engine( 'html', nunjucks.render ) ;
+// app.set('view engine', 'html');
 
 const server = app.listen(port, function(){
   console.log('Listening on port ' + port);
